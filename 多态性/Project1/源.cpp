b4a95a14ef6c94fd4f96bd81139526180ac09488
SA-6 <2652028269@qq.com>
2022-12-13 10:53:18 +0800

    多态性作业

---
+++ 多态性/Project1/源.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+class complex
+{
+public:
+	double real;
+	double image;
+public:
+	complex() {
+		real = 0; image = 0;
+	}
+	complex(double a, double b)
+	{
+		real = a;
+		image = b;
+	}
+	void display()
+	{
+		cout << "real is:" << real << ' ';
+		cout << "image is:" << image<<endl;
+	}
+	friend complex operator +(complex a, complex b)
+	{
+		return complex(a.real + b.real, a.image + b.image);
+	}
+	friend complex operator -(complex a, complex b)
+	{
+		return complex(a.real - b.real, a.image - b.image);
+	}
+	friend complex operator ++(complex &a)
+	{
+		return complex(++a.real , a.image );
+	}
+	friend complex operator ++(complex &a,int)
+	{
+		return complex(a.real++ , a.image );
+	}
+};
+/*class human
+{
+public:
+	int know;
+public:
+	human() {
+		know = 0;
+	}
+	void learn()
+	{
+		know++;
+	}
+};
+class student:public human
+{
+public:
+	void learn()
+	{
+		know += 2;
+	}
+};*/
+int main() {
+	complex comp1(3.1,2.7);
+	comp1.display();
+	complex comp2(2.0, 2.0);
+	comp2.display();
+	complex comp3=comp1 + comp2;
+	comp3.display();
+	comp3++;
+	comp3.display();
+}
